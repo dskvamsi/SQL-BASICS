0c4965c82d82ffe3a9289e86457314599f3146aa
dskvamsi <dskvamsi24@gmail.com>
2026-03-21 21:45:26 +0530

    fix: modified res_id

--- restarunts.sql
+++ restarunts.sql
@@ -13,10 +13,10 @@ CREATE TABLE restaurants (
 desc  restaurants;
 INSERT INTO restaurants (restaurant_id, restaurant_name, city, rating, opening_time, closing_time)
 VALUES 
-(1, 'Spicy Hub', 'Hyderabad', 4.5, '09:00:00', '22:00:00'),
-(2, 'Food Palace', 'Vijayawada', 4.2, '10:00:00', '23:00:00'),
-(3, 'Tasty Bites', 'Visakhapatnam', 4.7, '08:30:00', '21:30:00'),
-(4, 'Andhra Flavors', 'Guntur', 4.3, '09:30:00', '22:30:00'),
-(5, 'Urban Kitchen', 'Tirupati', 4.6, '10:00:00', '23:00:00');
+(1000001, 'Spicy Hub', 'Hyderabad', 4.5, '09:00:00', '22:00:00'),
+(2000002, 'Food Palace', 'Vijayawada', 4.2, '10:00:00', '23:00:00'),
+(3000003, 'Tasty Bites', 'Visakhapatnam', 4.7, '08:30:00', '21:30:00'),
+(4000004, 'Andhra Flavors', 'Guntur', 4.3, '09:30:00', '22:30:00'),
+(5000005, 'Urban Kitchen', 'Tirupati', 4.6, '10:00:00', '23:00:00');
 
 select * from restaurants;
